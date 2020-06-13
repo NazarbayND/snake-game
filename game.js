d000abc66561c562e73bef77b000b4d2daa198b1
NazarbayND <nazarbay.ktl@gmail.com>
2020-06-13 18:04:54 +0600

    begining

--- game.js
+++ game.js
@@ -1,6 +1,32 @@
- function main() {
-     let a = 2
-     let b = 3
-     let c = a * b
-     return c
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from 'snake.js'
+
+
+let lastRenderTime = 0
+import SNAKE_SPEED from 'snake.js'
+
+const gameBoard = document.getElementById('game-board')
+
+function main(currentTime) {
+     window.requestAnimationFrame(main)
+     const SecondSinceLastRender = (currentTime - lastRenderTime)
+     if (SecondSinceLastRender < 1 / SNAKE_SPEED) return
+     
+
+     console.log('Render')
+     lastRenderTime = currentTime
+
+
+    update()
+    draw()
  }
+
+ window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+}
+
+function draw() {
+    drawSnake(gameBoard)
+}
+
